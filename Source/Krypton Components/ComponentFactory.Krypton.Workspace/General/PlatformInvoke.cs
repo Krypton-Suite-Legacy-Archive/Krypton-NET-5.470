91bbbfc450027d8bfb1cd5ff523ca3ccd0e01dc7
Wagnerp <peterwagner@live.co.uk>
2018-01-07 18:58:16 +0000

    * Reupload of fresh binaries

--- Source/Krypton Components/ComponentFactory.Krypton.Workspace/General/PlatformInvoke.cs
+++ Source/Krypton Components/ComponentFactory.Krypton.Workspace/General/PlatformInvoke.cs
@@ -1,11 +1,11 @@
 ﻿// *****************************************************************************
 // 
-//  © Component Factory Pty Ltd 2017. All rights reserved.
+//  © Component Factory Pty Ltd 2018. All rights reserved.
 //	The software and associated documentation supplied hereunder are the 
 //  proprietary information of Component Factory Pty Ltd, 13 Swallows Close, 
 //  Mornington, Vic 3931, Australia and are supplied subject to licence terms.
 // 
-//  Version 4.5.0.0 	www.ComponentFactory.com
+//  Version 4.7.0.0 	www.ComponentFactory.com
 // *****************************************************************************
 
 namespace ComponentFactory.Krypton.Workspace
