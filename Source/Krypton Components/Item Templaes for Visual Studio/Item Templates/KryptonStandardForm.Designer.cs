3fbd3b779cf0bf0edfdad47bc29f6b11bc747299
Peter Wagner <peterwagner@live.co.uk>
2018-01-12 10:39:04 +0000

    * Incremental update

--- Source/Krypton Components/Item Templaes for Visual Studio/Item Templates/KryptonStandardForm.Designer.cs
+++ Source/Krypton Components/Item Templaes for Visual Studio/Item Templates/KryptonStandardForm.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace ItemTemplates
+﻿using ComponentFactory.Krypton.Toolkit;
+namespace ItemTemplates
 {
     partial class KryptonStandardForm
     {
@@ -29,12 +30,32 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.kryptonPanel1 = new KryptonPanel();
+            this.kryptonManager = new KryptonManager();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonManager kryptonManager;
     }
 }
 
--- Source/Krypton Components/Item Templaes for Visual Studio/Item Templates/KryptonStandardForm.cs
+++ Source/Krypton Components/Item Templaes for Visual Studio/Item Templates/KryptonStandardForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComponentFactory.Krypton.Toolkit;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +11,7 @@ using System.Windows.Forms;
 
 namespace ItemTemplates
 {
-    public partial class KryptonStandardForm : Form
+    public partial class KryptonStandardForm : KryptonForm
     {
         public KryptonStandardForm()
         {
