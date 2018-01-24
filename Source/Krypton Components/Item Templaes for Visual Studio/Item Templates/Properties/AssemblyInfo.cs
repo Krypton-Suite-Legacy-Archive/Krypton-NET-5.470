58fc70ba792a4ef580a54fb0af8209cf40f9537e
Peter Wagner <peterwagner@live.co.uk>
2018-01-24 09:08:04 +0000

    * Reupload

---
+++ Source/Krypton Components/Item Templaes for Visual Studio/Item Templates/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Item Templates")]
+[assembly: AssemblyDescription("Item Templates")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Component Factory Pty Ltd, Peter Wagner (aka Wagnerp) & Simon Coghlan (aka Smurf-IV)")]
+[assembly: AssemblyProduct("Item Templates")]
+[assembly: AssemblyCopyright("© Component Factory Pty Ltd, 2006-2018. Then modifications by Peter Wagner (aka Wagnerp) & Simon Coghlan (aka Smurf-IV) 2017-2018. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8eeee5f4-4869-415a-8f41-9864d5eba10e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.70.13.0")]
+[assembly: AssemblyFileVersion("4.70.13.0")]
+[assembly: AssemblyInformationalVersion("4.70.13.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
