399e2235a2841914f8e53ff8a5cfc285affa77fb
Peter Wagner <peterwagner@live.co.uk>
2018-01-24 08:46:20 +0000

    * Versions

--- Source/Krypton Components/ComponentFactory.Krypton.Design/Properties/AssemblyInfo.cs
+++ Source/Krypton Components/ComponentFactory.Krypton.Design/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("4.70.0.0")]
 [assembly: AssemblyFileVersion("4.70.0.0")]
-[assembly: AssemblyInformationalVersion("4.70.524.0")]
+[assembly: AssemblyInformationalVersion("4.70.525.0")]
 [assembly: AssemblyCopyright("© Component Factory Pty Ltd, 2006-2018. Then modifications by Peter Wagner (aka Wagnerp) & Simon Coghlan (aka Smurf-IV) 2017-2018. All rights reserved.")]
 [assembly: AssemblyProduct("Krypton Design")]
 [assembly: AssemblyDefaultAlias("ComponentFactory.Krypton.Design.dll")]
