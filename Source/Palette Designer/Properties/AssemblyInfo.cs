874134b6c826a490b04549a3bb099796516beb2f
Peter Wagner <peterwagner@live.co.uk>
2018-02-16 18:11:37 +0000

    * Minor changes

--- Source/Palette Designer/Properties/AssemblyInfo.cs
+++ Source/Palette Designer/Properties/AssemblyInfo.cs
@@ -17,8 +17,8 @@ using System.Security.Permissions;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("4.5.0.0")]
-[assembly: AssemblyFileVersion("4.5.0.0")]
+[assembly: AssemblyVersion("4.70.6000.0")]
+[assembly: AssemblyFileVersion("4.70.6000.0")]
 [assembly: AssemblyInformationalVersion("4.5.0.0")]
 [assembly: AssemblyCopyright("© Component Factory Pty Ltd 2012. All rights reserved.")]
 [assembly: AssemblyProduct("Palette Designer")]
