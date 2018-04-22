6e1f8d0bdb6e7526f874f402f269d524e80c88bb
Peter Wagner <peterwagner@live.co.uk>
2018-04-22 13:13:22 +0100

    * Failsafe

---
+++ Source/Krypton Toolkit Updater/Krypton Toolkit Updater/UI/UpdaterMainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace KryptonToolkitUpdater.UI
+{
+    partial class UpdaterMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdaterMainForm));
+            this.pnlBackground = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.kryptonManager1 = new ComponentFactory.Krypton.Toolkit.KryptonManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pnlBackground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlBackground
+            // 
+            this.pnlBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBackground.Location = new System.Drawing.Point(0, 0);
+            this.pnlBackground.Name = "pnlBackground";
+            this.pnlBackground.Size = new System.Drawing.Size(800, 450);
+            this.pnlBackground.TabIndex = 0;
+            // 
+            // UpdaterMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlBackground);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UpdaterMainForm";
+            this.Text = "UpdaterMainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pnlBackground)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel pnlBackground;
+        private ComponentFactory.Krypton.Toolkit.KryptonManager kryptonManager1;
+    }
+}
---
+++ Source/Krypton Toolkit Updater/Krypton Toolkit Updater/UI/UpdaterMainForm.cs
@@ -0,0 +1,12 @@
+﻿using ComponentFactory.Krypton.Toolkit;
+
+namespace KryptonToolkitUpdater.UI
+{
+    public partial class UpdaterMainForm : KryptonForm
+    {
+        public UpdaterMainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
