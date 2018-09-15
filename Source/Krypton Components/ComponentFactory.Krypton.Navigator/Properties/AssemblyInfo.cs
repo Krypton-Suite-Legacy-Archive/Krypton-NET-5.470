04db5567a308ac38422b3a91351b5741ab741b0e
Peter Wagner <peterwagner@live.co.uk>
2018-09-15 15:58:30 +0100

    * Minor code changes

--- Source/Krypton Components/ComponentFactory.Krypton.Navigator/Properties/AssemblyInfo.cs
+++ Source/Krypton Components/ComponentFactory.Krypton.Navigator/Properties/AssemblyInfo.cs
@@ -16,9 +16,9 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
 
-[assembly: AssemblyVersion("4.70.585.0")]
-[assembly: AssemblyFileVersion("4.70.585.0")]
-[assembly: AssemblyInformationalVersion("4.70.585.0")]
+[assembly: AssemblyVersion("4.70.587.0")]
+[assembly: AssemblyFileVersion("4.70.587.0")]
+[assembly: AssemblyInformationalVersion("4.70.587.0")]
 [assembly: AssemblyCopyright("© Component Factory Pty Ltd, 2006-2018. Then modifications by Peter Wagner (aka Wagnerp) & Simon Coghlan (aka Smurf-IV) 2017-2018. All rights reserved.")]
 [assembly: AssemblyProduct("Krypton Navigator")]
 [assembly: AssemblyDefaultAlias("ComponentFactory.Krypton.Navigator.dll")]
