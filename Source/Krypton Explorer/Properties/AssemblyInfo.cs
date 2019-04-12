7aa4557a42eb5ae9d2ea674217e3bbf4f06de931
Peter Wagner <peterwagner@live.co.uk>
2019-04-12 09:11:34 +0100

    * Setup file

--- Source/Krypton Explorer/Properties/AssemblyInfo.cs
+++ Source/Krypton Explorer/Properties/AssemblyInfo.cs
@@ -17,8 +17,8 @@ using System.Security.Permissions;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("17.10.6012.15")]
-[assembly: AssemblyFileVersion("17.10.6012.15")]
+[assembly: AssemblyVersion("17.470.6012.15")]
+[assembly: AssemblyFileVersion("17.470.6012.15")]
 [assembly: AssemblyInformationalVersion("4.5.12.0")]
 [assembly: AssemblyCopyright("© Component Factory Pty Ltd 2012 - 2019. All rights reserved.")]
 [assembly: AssemblyProduct("Krypton Explorer")]
